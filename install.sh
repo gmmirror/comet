c1447457453c8e77db46c70149e894f03a02966c
gmmirror <blackstar76776@gmail.com>
2026-04-02 21:06:48 +0000

    update

--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ if [[ -d "${PREFIX}/opt/comet" ]]; then
     command rm -rf "${PREFIX}/opt/comet"
 fi
 
+if [[ -x "${PREFIX}/bin/comet" ]]; then
+    command rm -f "${PREFIX}/bin/comet"
+fi
+
 command git clone --depth 1 \
     'https://github.com/Zeronetsec/Comet' \
     "${PREFIX}/tmp/comet"
